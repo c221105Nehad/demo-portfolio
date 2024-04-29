e87ff4b02fba649b73e33e5aa871446425a9b8b8
c221105Nehad <nehadinkiadhossain@gmail.com>
2024-04-29 22:03:18 +0600

    initial design completed

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ let cursor = document.getElementById('cursor');
 
 let cursorChild = document.getElementById('cursor-child');
 
-window.addEventListener('mousemove', function(dets) {
+window.addEventListener('mousemove', function (dets) {
 
-    gsap.to (cursor, {
+    gsap.to(cursor, {
         x: dets.x,
         y: dets.y,
     });
 
-    gsap.to (cursorChild, {
+    gsap.to(cursorChild, {
         x: dets.x,
         y: dets.y,
         delay: 0.1
     })
 });
 
-gsap.from ('.hero-greeting', {
+gsap.from('.hero-greeting', {
     x: -100,
     duration: 1,
     opacity: 0,
@@ -36,44 +36,50 @@ document.querySelector('.rotate-t').style.color = 'crimson';
 document.querySelector('.rotate-y').style.color = 'royalblue';
 
 // gsap.to ('.rotate-y', {
-//     paddingLeft: 10,
+//     rotate: '180deg',
 //     yoyo: true,
 //     repeat: -1,
 //     duration: 0.5,
 // });
 
-gsap.to ('.about-animation h1', {
-    transform: 'translateX(-180%)',
-    scrollTrigger: {
-        trigger: '.about-animation',
-        scroll: 'body',
-        start: 'top 0%',
-        end: 'top -400%',
-        scrub: 2,
-        pin: true
-    }
-});
+const overflowXEffect = () => {
 
-gsap.to ('.what-animation h1', {
-    transform: 'translateX(0%)',
-    scrollTrigger: {
-        trigger: '.what-animation',
-        scroll: 'body',
-        start: 'top 0%',
-        end: 'top -400%',
-        scrub: 2,
-        pin: true
-    }
-});
+    gsap.to('.about-animation h1', {
+        transform: 'translateX(-180%)',
+        scrollTrigger: {
+            trigger: '.about-animation',
+            scroll: 'body',
+            start: 'top 0%',
+            end: 'top -400%',
+            scrub: 2,
+            pin: true
+        }
+    });
 
-gsap.to ('.summery-animation h1', {
-    transform: 'translateX(-190%)',
-    scrollTrigger: {
-        trigger: '.summery-animation',
-        scroller: 'body',
-        start: 'top 0%',
-        end: 'top -300%',
-        scrub: 2,
-        pin: true
-    }
-});
+    gsap.to('.what-animation h1', {
+        transform: 'translateX(0%)',
+        scrollTrigger: {
+            trigger: '.what-animation',
+            scroll: 'body',
+            start: 'top 0%',
+            end: 'top -400%',
+            scrub: 2,
+            pin: true
+        }
+    });
+
+    gsap.to('.summery-animation h1', {
+        transform: 'translateX(-190%)',
+        scrollTrigger: {
+            trigger: '.summery-animation',
+            scroller: 'body',
+            start: 'top 0%',
+            end: 'top -300%',
+            scrub: 2,
+            pin: true
+        }
+    });
+
+}
+
+// overflowXEffect();
