86f4c37fa67e5d537732872b8fe0e961accb8192
c221105Nehad <nehadinkiadhossain@gmail.com>
2024-04-30 22:15:37 +0600

    almost done except hero section

--- script.js
+++ script.js
@@ -45,13 +45,13 @@ document.querySelector('.rotate-y').style.color = 'royalblue';
 const overflowXEffect = () => {
 
     gsap.to('.about-animation h1', {
-        transform: 'translateX(-180%)',
+        transform: 'translateX(-200%)',
         scrollTrigger: {
             trigger: '.about-animation',
             scroll: 'body',
             start: 'top 0%',
             end: 'top -400%',
-            scrub: 2,
+            scrub: 3,
             pin: true
         }
     });
@@ -82,18 +82,18 @@ const overflowXEffect = () => {
 
 }
 
-// overflowXEffect();
+overflowXEffect();
 
-gsap.from ('.about-container', {
+gsap.from('.about-container', {
     marginTop: '15vh',
     scale: 0.9,
-    duration: 1.5,
+    duration: 1,
     scrollTrigger: '.about-container',
     transform: 'translateY(30px)',
     opacity: 0.1
 });
 
-gsap.from ('.footer-container', {
+gsap.from('.footer-container', {
     transform: 'translateY(100%)',
     paddingTop: '0%',
     scrollTrigger: {
@@ -106,7 +106,7 @@ gsap.from ('.footer-container', {
 });
 
 
-gsap.from ('.left-section-title', {
+gsap.from('.left-section-title', {
     x: -60,
     opacity: 0,
     scrollTrigger: {
@@ -125,9 +125,79 @@ gsap.from ('.left-section-title', {
 // });
 
 
-gsap.from ('.form-title', {
+gsap.from('.form-title', {
     y: -70,
     duration: 1,
     opacity: 0,
     scrollTrigger: '.form-title'
+});
+
+let button = document.querySelector('input[type="button"]');
+
+button.addEventListener('mouseenter', function () {
+
+    gsap.to('.effect-element', {
+        transform: 'translateX(530%)',
+        backgroundColor: 'white',
+        delay: 0.5,
+        duration: 1
+    });
+});
+
+button.addEventListener('mouseleave', function () {
+
+    gsap.to('.effect-element', {
+        transform: 'translateX(-100%)',
+        backgroundColor: 'white',
+        duration: 1,
+        delay: 0.5,
+    });
+});
+
+gsap.to ('.resume', {
+    letterSpacing: '3.3vw',
+    duration: 0.7,
+    ease: "power1.in",
+    scrollTrigger: '.resume'
+});
+
+gsap.from ('.about-title', {
+    y: 40,
+    duration: 2,
+    delay: 0.5,
+    scrollTrigger: '.about-title',
+    ease: "elastic.out(1,0.3)",
+    opacity: 0
+});
+
+gsap.from ('.summery-content-left', {
+    marginTop: '500px',
+    duration: 1,
+    opacity: 0,
+    scrollTrigger: '.summery-content-left'
+});
+
+gsap.from ('.summery-content-right', {
+    marginTop: '500px',
+    duration: 1,
+    opacity: 0,
+    scrollTrigger: '.summery-content-right'
+});
+
+
+gsap.from ('.what-title', {
+    scale: 100,
+    duration: 1,
+    opacity: 0,
+    delay: 3,
+    scrollTrigger: '.what-title'
+});
+
+gsap.from ('.what-skills-card', {
+    transform: 'translateX(200%) rotateY(120deg)',
+    opacity: 0,
+    duration: 1.5,
+    scrollTrigger: '.what-skills-card',
+    stagger: 0.2,
+    delay: 1.5,
 });
