c7222fc50beb89f8dcb6f60e7ecf8dc5b89576a5
c221105Nehad <nehadinkiadhossain@gmail.com>
2024-05-01 22:02:36 +0600

    website done maybe I will do some more changes there

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+
 let cursor = document.getElementById('cursor');
 
 let cursorChild = document.getElementById('cursor-child');
+
+const parentUl = document.getElementById('parent-ul');
+
+
+let blog = document.querySelector('.blog');
 
 window.addEventListener('mousemove', function (dets) {
 
@@ -50,7 +56,7 @@ const overflowXEffect = () => {
             trigger: '.about-animation',
             scroll: 'body',
             start: 'top 0%',
-            end: 'top -400%',
+            end: 'top -500%',
             scrub: 3,
             pin: true
         }
@@ -62,8 +68,8 @@ const overflowXEffect = () => {
             trigger: '.what-animation',
             scroll: 'body',
             start: 'top 0%',
-            end: 'top -400%',
-            scrub: 2,
+            end: 'top -500%',
+            scrub: 3,
             pin: true
         }
     });
@@ -74,8 +80,8 @@ const overflowXEffect = () => {
             trigger: '.summery-animation',
             scroller: 'body',
             start: 'top 0%',
-            end: 'top -300%',
-            scrub: 2,
+            end: 'top -500%',
+            scrub: 3,
             pin: true
         }
     });
@@ -106,17 +112,17 @@ gsap.from('.footer-container', {
 });
 
 
-gsap.from('.left-section-title', {
-    x: -60,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: 'left-footer',
-        scroller: 'body',
-        start: 'top 40%',
-        end: 'top 50%',
-        scrub: 2
-    }
-});
+// gsap.from('.left-section-title', {
+//     x: -60,
+//     opacity: 0,
+//     scrollTrigger: {
+//         trigger: '.left-footer',
+//         scroller: 'body',
+//         start: 'top 40%',
+//         end: 'top 50%',
+//         scrub: 2
+//     }
+// });
 
 // gsap.from ('.left-section-title', {
 //     x: -60,
@@ -154,14 +160,14 @@ button.addEventListener('mouseleave', function () {
     });
 });
 
-gsap.to ('.resume', {
+gsap.to('.resume', {
     letterSpacing: '3.3vw',
     duration: 0.7,
     ease: "power1.in",
     scrollTrigger: '.resume'
 });
 
-gsap.from ('.about-title', {
+gsap.from('.about-title', {
     y: 40,
     duration: 2,
     delay: 0.5,
@@ -170,14 +176,14 @@ gsap.from ('.about-title', {
     opacity: 0
 });
 
-gsap.from ('.summery-content-left', {
+gsap.from('.summery-content-left', {
     marginTop: '500px',
     duration: 1,
     opacity: 0,
     scrollTrigger: '.summery-content-left'
 });
 
-gsap.from ('.summery-content-right', {
+gsap.from('.summery-content-right', {
     marginTop: '500px',
     duration: 1,
     opacity: 0,
@@ -185,19 +191,134 @@ gsap.from ('.summery-content-right', {
 });
 
 
-gsap.from ('.what-title', {
+gsap.from('.what-title', {
     scale: 100,
-    duration: 1,
     opacity: 0,
-    delay: 3,
-    scrollTrigger: '.what-title'
+    scrollTrigger: {
+        trigger: '.what-container',
+        scroller: 'body',
+        start: 'top 50%',
+        end: 'top 50%',
+        scrub: 1.5
+    }
 });
 
-gsap.from ('.what-skills-card', {
+gsap.from('.what-skills-card', {
     transform: 'translateX(200%) rotateY(120deg)',
     opacity: 0,
     duration: 1.5,
     scrollTrigger: '.what-skills-card',
-    stagger: 0.2,
-    delay: 1.5,
+    stagger: -0.2,
+    delay: 0.5,
+    ease: "bounce.out",
 });
+
+
+const forPortfolio = () => {
+
+    let portfolio = document.querySelector('.portfolio');
+
+    let portfolioText = portfolio.textContent;
+
+    let splittedTextPortfolio = portfolioText.split('');
+
+    let clutter = '';
+
+    splittedTextPortfolio.forEach(function (letter) {
+        clutter += `<span>${letter}</span>`;
+    });
+
+    portfolio.innerHTML = clutter;
+
+
+    portfolio.addEventListener('mouseover', (det) => {
+        gsap.to(portfolio.querySelectorAll('span'), {
+            y: '-20px',
+            duration: 0.7,
+            stagger: 0.15,
+            opacity: 0.5
+        });
+    });
+
+    portfolio.addEventListener('mouseout', () => {
+        gsap.to(portfolio.querySelectorAll('span'), {
+            y: 0,
+            duration: 0.7,
+            stagger: 0.15,
+            opacity: 1
+        });
+    });
+};
+
+forPortfolio();
+
+
+
+const forBlog = () => {
+
+    let blogText = blog.textContent;
+
+    let splittedTextBlog = blogText.split('');
+
+    let clutter = '';
+
+    splittedTextBlog.forEach(function (letter) {
+        clutter += `<span>${letter}</span>`;
+    });
+
+    blog.innerHTML = clutter;
+
+    blog.addEventListener('mouseover', (det) => {
+        gsap.to(blog.querySelectorAll('span'), {
+            y: '-20px',
+            duration: 0.7,
+            stagger: 0.15,
+            opacity: 0.5
+        });
+    });
+
+    blog.addEventListener('mouseout', () => {
+        gsap.to(blog.querySelectorAll('span'), {
+            y: 0,
+            duration: 0.7,
+            stagger: 0.15,
+            opacity: 1
+        });
+    });
+}
+
+forBlog();
+
+// Array.from(parentUl.children).forEach(function (li) {
+
+//     let anchorTag = document.querySelector('li>a');
+
+//     let anchorText = anchorTag.textContent;
+
+//     let splittedText = anchorText.split('');
+
+//     let clutter = '';
+
+//     splittedText.forEach(function (letter) {
+//         clutter += `<span>${letter}</span>`
+//     })
+
+//     console.log(clutter);
+
+//     anchorTag.innerHTML = clutter;
+
+//     li.addEventListener('mouseover', textEffect (anchorTag));
+
+//     const textEffect = (anchorTag)=> {
+//         gsap.from(anchorTag, {
+//             y: -30,
+//             opacity: 0,
+//             stagger: 0.15
+//         });
+//     }
+
+//     textEffect(anchorTag);
+
+// });
+
+
